5abd8376e401d6bdc6aea78895c284af877b5a06
Burak Tekin <tknbrk@gmail.com>
2020-10-25 13:29:39 +0100

    add configuration files for typescript and vue

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  /*
+  called in main.js currently not required!
+  css: {
+    loaderOptions: {
+      sass: {
+        prependData: `@import "@/assets/css/scaffolds.scss";`
+      }
+    }
+  },
+   */
+}
