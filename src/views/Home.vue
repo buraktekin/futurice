d3c2906ed64f33f9151ed6e5b6f5df8b863bedf5
Burak Tekin <tknbrk@gmail.com>
2020-10-25 13:39:08 +0100

    add parent components

---
+++ src/views/Home.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="home">
+    <Navbar/>
+    <Grid/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import Grid from '@/components/Grid.vue';
+import Navbar from '@/components/Navbar.vue';
+
+@Component({
+  name: 'Home',
+  components: {
+    Grid,
+    Navbar,
+  },
+  props: {
+    panel: {
+      type: Function,
+    },
+  },
+})
+export default class Home extends Vue {}
+</script>
+
+<style scoped>
+.home {
+  width: 100%;
+  height: 100%;
+  position: relative;
+  display: inline-flex !important;
+}
+</style>
