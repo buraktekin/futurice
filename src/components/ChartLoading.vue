d9c4dc467918aa8bdac5f9173deaa646a4ee9a84
Burak Tekin <tknbrk@gmail.com>
2020-10-25 13:48:42 +0100

    add loader component

---
+++ src/components/ChartLoading.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <sui-segment class="loading">
+    <sui-dimmer active>
+      <sui-loader size="large">Loading</sui-loader>
+    </sui-dimmer>
+  </sui-segment>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+};
+</script>
+<style scoped></style>
---
+++ src/components/Loading.vue
@@ -0,0 +1,30 @@
+<template lang="html">
+  <sui-segment class="loading">
+    <sui-dimmer active>
+      <sui-loader size="large">Loading</sui-loader>
+    </sui-dimmer>
+  </sui-segment>
+</template>
+
+<script>
+export default {
+  name: 'Loading',
+};
+</script>
+<style scoped>
+.loading {
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  top: -14px;
+  left: 0;
+  z-index: 100;
+  background: transparent !important;
+  box-shadow: none;
+  border: none;
+}
+.ui.loading.segment:before {
+  background: rgba(0, 0, 0, .5);
+  box-shadow: none;
+}
+</style>
