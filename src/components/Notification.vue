832c77f16714e08345da0a7ca180cbfb390912a7
Burak Tekin <tknbrk@gmail.com>
2020-10-25 13:45:29 +0100

    add notification (not active) and side panel to carry the feed

---
+++ src/components/Notification.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <transition name="fade">
+      <sui-message
+        v-if="visible"
+        header="Welcome back!"
+        :content="msg"
+        dismissable
+        @notify="notify"
+      />
+    </transition>
+    <p v-if="!visible">
+      Message will come back in 4s
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Notification',
+  data() {
+    return {
+      visible: false,
+      msg: '',
+    };
+  },
+  methods: {
+    notify() {
+      this.visible = true;
+      setTimeout(() => {
+        this.visible = false;
+      }, 4000);
+    },
+  },
+};
+</script>
+
+<style media="screen" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 1s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ src/components/SidePanel.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="'hq-sidepanel" v-if="isActive">
+    <FeedHeader/>
+    <Feed/>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue, Watch } from 'vue-property-decorator';
+import FeedHeader from '@/components/FeedHeader.vue';
+import Feed from '@/components/Feed.vue';
+import store from '@/store/index';
+
+@Component({
+  name: 'SidePanel',
+  components: {
+    FeedHeader,
+    Feed,
+  },
+})
+export default class SidePanel extends Vue {
+  constructor() {
+    super();
+  }
+
+  get isActive() {
+    return store.state.isSidePanelOpen;
+  }
+
+  public togglePanel() {
+    return store.dispatch('toggleSidePanel', !this.isActive);
+  }
+
+   // Methods
+}
+</script>
+
+<style lang="scss"></style>
